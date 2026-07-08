aa7d16bc8e2beca50faf563c544a3cefb410cadb
Lenka Nedeljkovic <lenkacned@users.noreply.github.com>
2026-07-08 14:41:35 +0200

    chore: initialize React JSON editor app

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+// uzmi div#root i ubaci React App unutra
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
